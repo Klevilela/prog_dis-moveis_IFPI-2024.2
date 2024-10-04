f8598e4767a4063763aeb518cb5c330f0d4cc058
Kleberson <klebisvillela@outlook.com>
2024-10-04 00:01:53 -0300

    teste

---
+++ teste.dart
@@ -0,0 +1,19 @@
+void main() {
+  const int x = 20;
+  const int y = 30;
+  dynamic nome;
+  nome = 15;
+  print(soma(x, y));
+  print(nome < 0);
+}
+
+int soma(int a, int b) {
+  return a + b;
+}
+
+
+class Pessoa {
+  String nome;
+  int idade;
+
+}
